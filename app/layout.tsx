a6232dcebeee4de4498d0ae2529fe0c05c6a9ae7
bithighlander <bithighlander@gmail.com>
2024-05-16 12:53:09 -0600

    pre-remove tailwind

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,9 +1,12 @@
 import "styles/tailwind.css"
+import { PioneerProvider } from "./providers";
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
+    <PioneerProvider>
       <body>{children}</body>
+    </PioneerProvider>
     </html>
   )
 }
